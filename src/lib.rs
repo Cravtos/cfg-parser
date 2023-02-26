a012db170c50cdbb65401218325b6fc3eed6b6af
Nikita Kryukov <cravtos92@gmail.com>
2023-02-26 16:25:58 +0700

    Implement analyze

--- src/lib.rs
+++ src/lib.rs
@@ -1,52 +1,139 @@
-// Недетерминированный синтаксический анализ с возвратами
-
-// Задана следующая порождающая грамматика:
-// T = { !, +, *, (, ), a, b }
-// N = { А, В, Т, М }, A - стартовый символ
-
-// Продукции:
-// 1) А -> !В!
-// 2) В -> Т
-// 3) В -> Т + В
-// 4) Т -> М
-// 5) Т -> М * Т
-// 6) М -> a
-// 7) М -> b
-// 8) М -> (В)
-
-// Примеры работы программы:
-
-// 1. !a+b!
-// Нисходящий: 1 3 4 6 2 4 7
-// Восходящий: 6 4 7 4 2 3 1
-
-// TODO: сделать тесты
-// TOOD: сделать вбивание правил вывода
-// TODO: разобрать алгоритм восходящего/нисходящего
+use std::collections::HashSet;
 
 #[derive(Debug)]
 pub struct Prods {
-    terms: Vec<char>,
-    nonterms: Vec<char>,
+    terms: HashSet<char>,
+    nonterms: HashSet<char>,
     init: char,
-    rules: Vec<(char, Vec<char>)>
+    pub rules: Vec<(char, Vec<char>)>,
+}
+
+#[derive(Clone, Debug)]
+struct Derivation {
+    pub idxs: Vec<usize>,
+    str_pos: usize,
+}
+
+#[derive(Clone, Debug)]
+struct State {
+    sym_pos: usize,
+    from_rule: usize,
+    drv: Derivation,
+}
+
+enum Result {
+    Match(Derivation),
+    NotMatched,
+    NoRules,
 }
 
 impl Prods {
     pub fn new(terms: &[char], nonterms: &[char], init: char) -> Self {
-        Prods { 
-            terms: terms.to_vec(),
-            nonterms: nonterms.to_vec(),
+        Prods {
+            terms: HashSet::from_iter(terms.iter().cloned()),
+            nonterms: HashSet::from_iter(nonterms.iter().cloned()),
             init,
             rules: vec![],
         }
     }
 
+    // TODO: check that from and to is from terms or nonterms
     pub fn add_rule(&mut self, from: char, to: &[char]) {
         self.rules.push((from, to.to_vec()))
     }
 
-    pub fn analyze(&self, s: &str) -> &[usize] {
-        unimplemented!();
+    fn handle_non_term(&self, rhs: &Vec<char>, s: &str, rule: usize) -> Result {
+        let mut prev_states = vec![];
+
+        let mut state = State {
+            sym_pos: 0,
+            from_rule: 0,
+            drv: Derivation { idxs: vec![rule], str_pos: 0 }
+        };
+
+        while state.sym_pos < rhs.len() {
+            if self.nonterms.contains(&rhs[state.sym_pos]) {
+                prev_states.push(state.clone());
+            }
+
+            match self._analyze(rhs[state.sym_pos], &s[state.drv.str_pos..], state.from_rule) {
+                Result::Match(mut drv) => {
+                    state.drv.idxs.append(&mut drv.idxs);
+                    state.drv.str_pos += drv.str_pos;
+                    state.sym_pos += 1;
+                    state.from_rule = 0;
+                },
+                Result::NotMatched => {
+                    // TODO: move to outer fn "restore_prev_state"
+                    state = if let Some(prev_state) = prev_states.pop() { 
+                        prev_state 
+                    } else {
+                        return Result::NotMatched;
+                    };
+                    state.from_rule += 1;
+                },
+                Result::NoRules => {
+                    if prev_states.pop().is_none() { 
+                        return Result::NotMatched;
+                    };
+
+                    state = if let Some(prev_state) = prev_states.pop() { 
+                        prev_state 
+                    } else {
+                        return Result::NotMatched;
+                    };
+                    state.from_rule += 1;
+                }
+            };
+        }
+
+        return Result::Match(state.drv);
+    }
+
+    fn _analyze(&self, cur: char, s: &str, from_rule: usize) -> Result {
+        if self.nonterms.contains(&cur) {
+            for (i, (_, rhs)) in self
+                .rules
+                .iter()
+                .enumerate()
+                .skip(from_rule)
+                .filter(|rule| rule.1 .0 == cur)
+            {
+                println!("{i}. {cur} -> {rhs:?}");
+                match self.handle_non_term(rhs, s, i) {
+                    Result::Match(drv) => {
+                        return Result::Match(drv);
+                    },
+                    _ => (),
+                }
+            }
+        }
+
+        if self.terms.contains(&cur) {
+            let ch = match s.chars().next() {
+                Some(ch) => ch,
+                None => return Result::NotMatched,
+            };
+
+            if ch == cur {
+                return Result::Match(Derivation {
+                    idxs: vec![],
+                    str_pos: 1,
+                });
+            }
+
+            return Result::NotMatched;
+        }
+
+        Result::NoRules
+    }
+
+    // FIXME: возмоно зацикливание для леворекурсивной грамматики
+    pub fn analyze(&self, s: &str) -> Option<Vec<usize>> {
+        let cur = self.init;
+        match self._analyze(cur, s, 0) {
+            Result::Match(drv) => Some(drv.idxs),
+            _ => None,
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,25 @@ use parser::Prods;
 
 fn main() {
     let terms = ['!', '+', '*', '(', ')', 'a', 'b'];
-    let nonterms = ['А', 'В', 'Т', 'М'];
+    let nonterms = ['A', 'B', 'T', 'M'];
     let init = 'A';
 
     let mut prods = Prods::new(&terms, &nonterms, init);
-
     prods.add_rule('A', &['a']);
-    prods.add_rule('А', &['!', 'В', '!']);
-    prods.add_rule('В', &['Т']);
-    prods.add_rule('В', &['Т', '+', 'В']);
-    prods.add_rule('Т', &['М']);
-    prods.add_rule('Т', &['М', '*', 'Т']);
-    prods.add_rule('М', &['a']);
-    prods.add_rule('М', &['b']);
-    prods.add_rule('М', &['(', 'В', ')']);
+    prods.add_rule('A', &['!', 'B', '!']);
+    prods.add_rule('B', &['T']);
+    prods.add_rule('B', &['T', '+', 'B']);
+    prods.add_rule('T', &['M']);
+    prods.add_rule('T', &['M', '*', 'T']);
+    prods.add_rule('M', &['a']);
+    prods.add_rule('M', &['b']);
+    prods.add_rule('M', &['(', 'B', ')']);
 
-    let input = "!B!";
+    for (i, rule) in prods.rules.iter().enumerate() {
+        println!("{}) {} -> {}", i, rule.0, String::from_iter(rule.1.iter()));
+    }
+
+    let input = "!a+b!";
     let derivation = prods.analyze(input);
     println!("{:?}", derivation);
 }
