f5c73c166ffa9f4c596c0c8d68aa19ca84a84400
Nikita Kryukov <cravtos92@gmail.com>
2023-06-09 23:50:14 +0700

    add lr1 analyzer

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,39 @@
+use std::collections::HashMap;
 use std::collections::HashSet;
+
+type ControlTable = HashMap<char, Vec<Action>>;
 
 pub struct Prods {
     terms: HashSet<char>,
     nonterms: HashSet<char>,
     init: char,
     pub rules: Vec<(char, Vec<char>)>,
+    table: ControlTable,
 }
 
-#[derive(PartialEq, Eq)]
-enum State {
-    Normal,
-    Reverse,
-    Ended,
-}
-
-#[derive(PartialEq, Eq, Debug)]
-enum Record {
-    Carry,
+#[derive(PartialEq, Eq, Debug, Clone)]
+pub enum Action {
+    Carry(usize),
     Rule(usize),
+    Goto(usize),
+    Error,
+    Exit,
+}
+
+#[derive(PartialEq, Eq, Debug, Clone)]
+pub enum Element {
+    Symbol(char),
+    State(usize),
 }
 
 impl Prods {
-    pub fn new(terms: &[char], nonterms: &[char], init: char) -> Self {
+    pub fn new(terms: &[char], nonterms: &[char], init: char, table: ControlTable) -> Self {
         Prods {
             terms: HashSet::from_iter(terms.iter().cloned()),
             nonterms: HashSet::from_iter(nonterms.iter().cloned()),
             init,
             rules: vec![],
+            table,
         }
     }
 
@@ -34,165 +41,158 @@ impl Prods {
         self.rules.push((from, to.to_vec()))
     }
 
-    /// Восходящий анализ.
-    /// Грамматика должны быть без циклов и неукорачивающейся.
+    /// LR(1)
     pub fn analyze(&self, s: &str) -> Option<Vec<usize>> {
-        let mut deriv: Vec<char> = Vec::new();
-        let mut hist: Vec<Record> = Vec::new();
-        let mut idx = 0;
-        let mut state = State::Normal;
+        let mut stack: Vec<Element> = vec![Element::State(0)];
+        let mut production: Vec<usize> = Vec::new();
 
+        let mut state: usize;
+        let mut idx: usize = 0;
+        let mut symbol: char;
         loop {
-            match state {
-                State::Normal => state = self.handle_normal(&mut deriv, &mut hist, s, &mut idx),
-                State::Reverse => state = self.handle_reverse(&mut deriv, &mut hist, s, &mut idx),
-                State::Ended => return self.handle_ended(&mut deriv, &mut hist),
+            symbol = '$';
+            if idx < s.len() {
+                symbol = s.chars().nth(idx).unwrap();
+            }
+
+            match stack.last().expect("there should be element") {
+                Element::State(s) => state = *s,
+                _ => panic!("no state on top of stack!"),
+            }
+
+            let action = self.get_action(state, symbol);
+            match action {
+                Action::Carry(state) => {
+                    stack.push(Element::Symbol(symbol));
+                    stack.push(Element::State(state));
+                    idx += 1;
+                }
+                Action::Rule(rule) => {
+                    production.push(rule - 1);
+                    let (nonterm, terms) = &self.rules[rule - 1];
+                    for _ in 0..2 * terms.len() {
+                        stack.pop();
+                    }
+                    match stack.last().expect("there should be element") {
+                        Element::State(s) => state = *s,
+                        _ => panic!("no state on top of stack!"),
+                    }
+                    stack.push(Element::Symbol(*nonterm));
+                    match self.get_action(state, *nonterm) {
+                        Action::Goto(s) => {
+                            state = s;
+                            stack.push(Element::State(state));
+                        }
+                        _ => panic!("should be goto action!"),
+                    }
+                }
+                Action::Exit => break,
+                Action::Error => return None,
+                Action::Goto(_) => panic!("should meet goto"),
             }
         }
+
+        Some(production)
     }
 }
 
 // Helper functions
 impl Prods {
-    fn get_conv(&self, deriv: &[char], from: usize) -> Option<usize> {
-        for (i, (_, right)) in self.rules.iter().enumerate().skip(from) {
-            if deriv.ends_with(right) {
-                return Some(i);
-            }
-        }
-
-        None
-    }
-
-    fn handle_normal(
-        &self,
-        deriv: &mut Vec<char>,
-        hist: &mut Vec<Record>,
-        s: &str,
-        idx: &mut usize,
-    ) -> State {
-        // (1) – try convolute while possible
-        while let Some(rule) = self.get_conv(deriv, 0) {
-            let (left, right) = &self.rules[rule];
-            for _ in 0..right.len() {
-                deriv.pop();
-            }
-            deriv.push(*left);
-            hist.push(Record::Rule(rule));
-        }
-
-        // (2) – carry
-        if *idx < s.len() {
-            let sym = match s.chars().nth(*idx) {
-                Some(sym) => sym,
-                None => return State::Ended,
-            };
-            deriv.push(sym);
-            *idx += 1;
-
-            hist.push(Record::Carry);
-            return State::Normal;
-        }
-
-        // (3) – end
-        if deriv.len() == 1 && deriv[0] == self.init {
-            return State::Ended;
-        }
-
-        State::Reverse
-    }
-
-    fn handle_reverse(
-        &self,
-        deriv: &mut Vec<char>,
-        hist: &mut Vec<Record>,
-        s: &str,
-        idx: &mut usize,
-    ) -> State {
-        // (5а) – pop number and rule and find rule with a higher number matching stack
-        match deriv.pop() {
-            Some(_) => (),
-            None => return State::Ended,
-        };
-        let record = match hist.pop() {
-            Some(record) => record,
-            None => return State::Ended,
-        };
-        let rule = match record {
-            Record::Rule(rule) => rule,
-            Record::Carry => {
-                // (5g) – reverse by terminal
-                if *idx == 0 {
-                    // Doesn't belong to grammar
-                    return State::Ended;
-                }
-                *idx -= 1;
-                return State::Reverse;
-            }
-        };
-
-        let (_, right) = &self.rules[rule];
-        for &c in right {
-            deriv.push(c);
-        }
-
-        match self.get_conv(deriv, rule + 1) {
-            Some(rule) => {
-                let (left, right) = &self.rules[rule];
-                for _ in 0..right.len() {
-                    deriv.pop();
-                }
-                deriv.push(*left);
-                hist.push(Record::Rule(rule));
-                State::Normal
-            }
-            None => {
-                // (5b) – everything read and can't find another rule
-                if *idx >= s.len() {
-                    // already done everything needed
-                    return State::Reverse;
-                }
-
-                // (5c) – reverse with carry
-                let sym = match s.chars().nth(*idx) {
-                    Some(sym) => sym,
-                    None => return State::Ended,
-                };
-                deriv.push(sym);
-                *idx += 1;
-
-                hist.push(Record::Carry);
-                State::Normal
-            }
-        }
-    }
-
-    fn handle_ended(&self, deriv: &mut Vec<char>, hist: &mut Vec<Record>) -> Option<Vec<usize>> {
-        if deriv.len() != 1 || deriv[0] != self.init {
-            return None;
-        }
-
-        let mut result = Vec::new();
-        for record in hist.iter() {
-            match record {
-                Record::Rule(rule) => result.push(*rule),
-                Record::Carry => (),
-            }
-        }
-        Some(result)
+    fn get_action(&self, state: usize, symbol: char) -> Action {
+        self.table.get(&symbol).expect("Symbol is correct")[state].clone()
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::collections::HashMap;
+
+    fn control_table() -> ControlTable {
+        let mut table: ControlTable = HashMap::new();
+        const SYMBOLS: [char; 12] = ['!', '+', '*', '(', ')', 'a', 'b', 'A', 'B', 'T', 'M', '$'];
+        for symbol in SYMBOLS {
+            table.insert(symbol, vec![Action::Error; 16]);
+        }
+        table.get_mut(&'!').unwrap()[0] = Action::Carry(2);
+        table.get_mut(&'A').unwrap()[0] = Action::Goto(1);
+
+        table.get_mut(&'$').unwrap()[1] = Action::Exit;
+
+        table.get_mut(&'(').unwrap()[2] = Action::Carry(3);
+        table.get_mut(&'a').unwrap()[2] = Action::Carry(4);
+        table.get_mut(&'b').unwrap()[2] = Action::Carry(5);
+        table.get_mut(&'B').unwrap()[2] = Action::Goto(6);
+        table.get_mut(&'T').unwrap()[2] = Action::Goto(7);
+        table.get_mut(&'M').unwrap()[2] = Action::Goto(8);
+
+        table.get_mut(&'(').unwrap()[3] = Action::Carry(3);
+        table.get_mut(&'a').unwrap()[3] = Action::Carry(4);
+        table.get_mut(&'b').unwrap()[3] = Action::Carry(5);
+        table.get_mut(&'B').unwrap()[3] = Action::Goto(9);
+        table.get_mut(&'T').unwrap()[3] = Action::Goto(7);
+        table.get_mut(&'M').unwrap()[3] = Action::Goto(8);
+
+        table.get_mut(&'!').unwrap()[4] = Action::Rule(6);
+        table.get_mut(&'+').unwrap()[4] = Action::Rule(6);
+        table.get_mut(&'*').unwrap()[4] = Action::Rule(6);
+        table.get_mut(&')').unwrap()[4] = Action::Rule(6);
+
+        table.get_mut(&'!').unwrap()[5] = Action::Rule(7);
+        table.get_mut(&'+').unwrap()[5] = Action::Rule(7);
+        table.get_mut(&'*').unwrap()[5] = Action::Rule(7);
+        table.get_mut(&')').unwrap()[5] = Action::Rule(7);
+
+        table.get_mut(&'!').unwrap()[6] = Action::Carry(10);
+
+        table.get_mut(&'!').unwrap()[7] = Action::Rule(2);
+        table.get_mut(&'+').unwrap()[7] = Action::Carry(11);
+        table.get_mut(&')').unwrap()[7] = Action::Rule(2);
+
+        table.get_mut(&'!').unwrap()[8] = Action::Rule(4);
+        table.get_mut(&'+').unwrap()[8] = Action::Rule(4);
+        table.get_mut(&'*').unwrap()[8] = Action::Carry(12);
+        table.get_mut(&')').unwrap()[8] = Action::Rule(4);
+
+        table.get_mut(&')').unwrap()[9] = Action::Carry(13);
+
+        table.get_mut(&'$').unwrap()[10] = Action::Rule(1);
+
+        table.get_mut(&'(').unwrap()[11] = Action::Carry(3);
+        table.get_mut(&'a').unwrap()[11] = Action::Carry(4);
+        table.get_mut(&'b').unwrap()[11] = Action::Carry(5);
+        table.get_mut(&'B').unwrap()[11] = Action::Goto(14);
+        table.get_mut(&'T').unwrap()[11] = Action::Goto(7);
+        table.get_mut(&'M').unwrap()[11] = Action::Goto(8);
+
+        table.get_mut(&'(').unwrap()[12] = Action::Carry(3);
+        table.get_mut(&'a').unwrap()[12] = Action::Carry(4);
+        table.get_mut(&'b').unwrap()[12] = Action::Carry(5);
+        table.get_mut(&'T').unwrap()[12] = Action::Goto(15);
+        table.get_mut(&'M').unwrap()[12] = Action::Goto(8);
+
+        table.get_mut(&'!').unwrap()[13] = Action::Rule(8);
+        table.get_mut(&'+').unwrap()[13] = Action::Rule(8);
+        table.get_mut(&'*').unwrap()[13] = Action::Rule(8);
+        table.get_mut(&')').unwrap()[13] = Action::Rule(8);
+
+        table.get_mut(&'!').unwrap()[14] = Action::Rule(3);
+        table.get_mut(&')').unwrap()[14] = Action::Rule(3);
+
+        table.get_mut(&'!').unwrap()[15] = Action::Rule(5);
+        table.get_mut(&'+').unwrap()[15] = Action::Rule(5);
+        table.get_mut(&')').unwrap()[15] = Action::Rule(5);
+
+        table
+    }
 
     fn init() -> Prods {
         let terms = ['!', '+', '*', '(', ')', 'a', 'b'];
         let nonterms = ['A', 'B', 'T', 'M'];
         let init = 'A';
+        let table = control_table();
 
-        let mut prods = Prods::new(&terms, &nonterms, init);
+        let mut prods = Prods::new(&terms, &nonterms, init, table);
         prods.add_rule('A', &['!', 'B', '!']);
         prods.add_rule('B', &['T']);
         prods.add_rule('B', &['T', '+', 'B']);
@@ -202,14 +202,14 @@ mod tests {
         prods.add_rule('M', &['b']);
         prods.add_rule('M', &['(', 'B', ')']);
 
-        for (i, rule) in prods.rules.iter().enumerate() {
-            println!(
-                "{}) {} -> {}",
-                i + 1,
-                rule.0,
-                String::from_iter(rule.1.iter())
-            );
-        }
+        // for (i, rule) in prods.rules.iter().enumerate() {
+        //     println!(
+        //         "{}) {} -> {}",
+        //         i + 1,
+        //         rule.0,
+        //         String::from_iter(rule.1.iter())
+        //     );
+        // }
 
         prods
     }
